529c9275e34f6034dcd9cbc9c9da1360ae7aa262
unreformable <danieltroszczynski@wp.pl>
2024-04-21 22:57:56 +0200

    Add project files.

---
+++ Bookshelf/Models/BookshelfContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bookshelf.Models
+{
+    public class Bookshelfcontext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite(@"Data Source=D:\bookshelf.db");
+    }
+}
