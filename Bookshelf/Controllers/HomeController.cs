b452c340a1046d61ece60c09c84247340b54f5f6
PKacper <264332@student.pwr.edu.pl>
2024-04-29 14:14:44 +0200

    Register and login features

--- Bookshelf/Controllers/HomeController.cs
+++ Bookshelf/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Bookshelf.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Drawing.Printing;
 
 namespace Bookshelf.Controllers
 {
@@ -28,40 +29,32 @@ namespace Bookshelf.Controllers
             return View();
         }
 
-        public IActionResult TryLogin(/*LoginViewModel model*/)
-        {
-            // Authenticate user
-            if (true/* authentication successful */)
-            {
-                // Redirect to specific page
-                return RedirectToAction("Index", "Account"); // Redirect to Account/Index
-            }
-            else
-            {
-                return RedirectToAction("Login", "Home"); // Refresh login page
-            }
-        }
-
         public IActionResult Register()
         {
             return View();
         }
-
-        public IActionResult TryRegister()
+        //Sprawdza, czy istnieje u¿ytkownik o podanym loginie i haœle, jeœli tak: zwraca stronê g³ówn¹, jeœli nie: zwraca ekran logowania.
+        //Do dodania: Potrzebna jakaœ zmienna, która bêdzie trzymaæ dane o zalogowanym u¿ytkowniku przez resztê sesji.
+        public IActionResult TryLogin(string Login, string Password)
         {
-            // Try register user
-            if (true/* registeration successful */)
+            List<User> users = new List<User>();
+
+            using(var db = new Bookshelfcontext())
             {
-                // Redirect to specific page
-                return RedirectToAction("SuccessfullRegister", "Home");
+                users = db.Users.Where(u => u.Login.Contains(Login) && u.Password.Contains(Password)).ToList();
+            }
+            if (users.Any())
+            {
+                return View("Index");
             }
             else
             {
-                return RedirectToAction("Index", "Register"); // Refresh login page
+                return View("Login");
             }
         }
-
-        public IActionResult SuccessfullRegister(User user)
+        //Dodaje u¿ytkownika do bazy danych i zwraca ekran udanej rejestracji.
+        //Do dodania: Sprawdzanie, czy wszystkie pola s¹ wype³nione
+        public IActionResult TryRegister(User user)
         {
             using (var db = new Bookshelfcontext())
             {
@@ -70,7 +63,7 @@ namespace Bookshelf.Controllers
                 db.Add(user);
                 db.SaveChanges();
             }
-            return View();
+            return View("SuccessfullRegister");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
